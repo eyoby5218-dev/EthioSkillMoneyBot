942fdd569be0948fedf237a33c47d9ed5b4b28db
eyoby5218-dev <eyoby5218@gmail.com>
2026-07-24 17:15:30 +0300

    Initialize SkillMoney Bot with greeting functionality

---
+++ bot.py
@@ -0,0 +1,11 @@
+
+print("SkillMoney Bot ተጀምሯል!")
+
+while True:
+    user = input("ስምህን አስገባ: ")
+
+    if user.lower() == "exit":
+        print("Bot ተዘጋ")
+        break
+
+    print("ሰላም " + user + "! እንኳን ወደ SkillMoney Bot በደህና መጣህ")
